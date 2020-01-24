25ce3c333171aa876bc7aeba28083136e90286bd
Kevin Wang <bird831205@hotmail.com>
2020-01-24 11:31:07 -0500

    attempt at deleting a recipe

--- src/main/java/com/github/kevinw831205/service/RecipeService.java
+++ src/main/java/com/github/kevinw831205/service/RecipeService.java
@@ -1,13 +1,12 @@
 package com.github.kevinw831205.service;
 
-import com.github.kevinw831205.model.CategoryTag;
-import com.github.kevinw831205.model.Recipe;
-import com.github.kevinw831205.repository.CategoryTagRepository;
-import com.github.kevinw831205.repository.RecipeRepository;
+import com.github.kevinw831205.model.*;
+import com.github.kevinw831205.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -15,6 +14,9 @@ import java.util.stream.StreamSupport;
 public class RecipeService {
     private RecipeRepository recipeRepository;
     private CategoryTagRepository categoryTagRepository;
+    private RatingRepository ratingRepository;
+    private InstructionRepository instructionRepository;
+    private IngredientRepository ingredientRepository;
 
     @Autowired
     public RecipeService(RecipeRepository recipeRepository, CategoryTagRepository categoryTagRepository) {
@@ -56,6 +58,23 @@ public class RecipeService {
 
     public Recipe delete(Long id) {
         Recipe recipe = findById(id);
+//        Set<CategoryTag> tags = recipe.getCategoryTags();
+//        for(CategoryTag tag:tags){
+//            categoryTagRepository.delete(tag);
+//        }
+//        List<Rating> ratings = recipe.getRatings();
+//        for(Rating r : ratings){
+//            ratingRepository.delete(r);
+//        }
+//        List<Instruction> instructions = recipe.getInstructionList();
+//        for(Instruction i : instructions){
+//            instructionRepository.delete(i);
+//        }
+//        List<Ingredient> ingredients = recipe.getIngredientList();
+//        for(Ingredient i : ingredients){
+//            ingredientRepository.delete(i);
+//        }
+
         recipeRepository.delete(recipe);
         return recipe;
     }
