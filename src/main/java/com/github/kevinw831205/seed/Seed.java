8068cd8fb38eaad9a3c20b431a1ffae5103c59ef
Kevin Wang <bird831205@hotmail.com>
2020-02-14 09:33:01 -0500

    seeding tags

--- src/main/java/com/github/kevinw831205/seed/Seed.java
+++ src/main/java/com/github/kevinw831205/seed/Seed.java
@@ -1,23 +1,31 @@
 package com.github.kevinw831205.seed;
 
 import com.github.kevinw831205.model.Account;
+import com.github.kevinw831205.model.CategoryTag;
 import com.github.kevinw831205.repository.AccountRepository;
+import com.github.kevinw831205.repository.CategoryTagRepository;
 import com.github.kevinw831205.security.MD5;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Seed {
     private AccountRepository accountRepository;
+    private CategoryTagRepository categoryTagRepository;
 
     @Autowired
-    public Seed(AccountRepository accountRepository) {
+    public Seed(AccountRepository accountRepository, CategoryTagRepository categoryTagRepository) {
         this.accountRepository = accountRepository;
+        this.categoryTagRepository = categoryTagRepository;
     }
 
     @EventListener
-    public void seed(ContextRefreshedEvent event) {
+    public void seed(ApplicationReadyEvent event) {
         seedAccountTable();
+        seedTags();
     }
 
     private void seedAccountTable() {
@@ -33,10 +41,19 @@ public class Seed {
         demoUser.setPassword(MD5.getMd5("demo123"));
         demoUser.setProfileImageUrl("https://via.placeholder.com/150");
         demoUser.setAdmin(true);
-        this.accountRepository.save(demoUser);
+        this.accountRepository.save(demoAdmin);
+    }
 
-
-
+    private void seedTags(){
+        CategoryTag tag = new CategoryTag();
+        tag.setName("breakfast");
+        categoryTagRepository.save(tag);
+        tag.setName("italian");
+        categoryTagRepository.save(tag);
+        tag.setName("steak");
+        categoryTagRepository.save(tag);
+        tag.setName("asian");
+        categoryTagRepository.save(tag);
     }
 
 }
